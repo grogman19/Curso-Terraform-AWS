7bb7c435e7912feca81d05096d596933c6e45e07
Asier Ibanez <asier.ibanez@gmail.com>
2022-10-20 18:25:58 +0200

    Elimino el data de RHEL

--- Ejercicio 1/main.tf
+++ Ejercicio 1/main.tf
@@ -88,28 +88,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# Definimos una estructura data para obtener la info de una imagen AMI RedHat de AWS
-data "aws_ami" "rhel_8_5" {
-    most_recent = true
-    owners = ["309956199498"] // Red Hat's Account ID
-    filter {
-        name   = "name"
-        values = ["RHEL-8.5*"]
-    }
-    filter {
-        name   = "architecture"
-        values = ["x86_64"]
-    }
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
 # Definimos un recurso de instancia EC2
 resource "aws_instance" "web" {
     ami = data.aws_ami.ubuntu.id
